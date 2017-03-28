8687567286eaca0c1dead12278731066448db68b
wshuang6 <seraph617@gmail.com>
2017-03-28 12:04:10 -0400

    setup

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ module.exports = {
     DEV: {
         client: 'pg',
         // ElephantSQL Config
-        // connection:'postgres://qvddfhyu:zyxUNHQzhz0Vd6J0lM@stampy.db.elephantsql.com:5432/qvddfhyu'
-        connection: {
-            user: 'dev',
-            password: 'letmein',
-            database: 'dev-restaurants-app'
-        },
+        connection:'postgres://isykopzh:UWj_8Krz1QP8rpJDhwq8jplMbY513zWe@stampy.db.elephantsql.com:5432/isykopzh',
+        // connection: {
+        //     user: 'dev',
+        //     password: 'letmein',
+        //     database: 'dev-restaurants-app'
+        // },
         // enable debug mode
-        // debug: true
+        debug: true
         
     },
     TEST: {
